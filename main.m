febc2c6d774c0f415987dd2c47ede5cf3af0518f
Stanley Zheng <szheng31@stuy.edu>
2022-08-04 14:00:46 -0400

    moved face width and height to main.m

--- main.m
+++ main.m
@@ -1,8 +1,83 @@
 clear, clc, close all
-
+%eyes
 [eyesWidth, eyesHeight] = getSizeOfGrayscaleFeature('EyePairBig', false)
+
+%mouth
 [mouthWidth, mouthHeight] = getSizeOfGrayscaleFeature('Mouth', true)
+
+%nose
 [noseWidth, noseHeight] = getSizeOfGrayscaleFeature('Nose', false)
+
+%face width and height
+videoReader = VideoReader('testvideos/video1.mp4');
+videoFrame      = readFrame(videoReader);
+
+
+faceNoseDetector = vision.CascadeObjectDetector("ClassificationModel",'Nose',"MaxSize",[50 50]);
+bboxNose            = step(faceNoseDetector, videoFrame);
+
+
+% Draw the returned bounding box around the det ected face.
+videoFrameNose = insertShape(videoFrame, 'Rectangle', bboxNose);
+
+
+
+bwFace = im2bw(videoFrameNose);
+figure
+subplot(3,1,1)
+imshow(bwFace);
+featureImage = imcrop(videoFrameNose,bboxNose);
+subplot(3,1,2)
+imshow(featureImage)
+
+
+noseLeft = [bboxNose(1) , bboxNose(2)];
+noseRight = [(bboxNose(1)+bboxNose(3)) , bboxNose(2)];
+
+
+for i=noseLeft(1):-1:1
+    if bwFace(noseLeft(2), i) == 0
+        leftEdge = [i,noseLeft(2)];
+        break
+    end
+end
+
+sizeFace = size(bwFace);
+for i=noseRight(1):sizeFace(2)
+    if bwFace(noseRight(2),i) == 0
+        rightEdge = [i,noseRight(2)];
+        break
+    end
+end
+
+%face height
+faceDetector = vision.CascadeObjectDetector();
+
+bboxFace = step(faceDetector, videoFrame);
+
+videoFrame = insertShape(videoFrame, 'Rectangle', bboxFace);
+subplot(3,1,3)
+imshow(videoFrame); title('Detected face');
+
+faceHeight = bboxFace(4)
+
+
+if ~exist("leftEdge")
+    disp(bboxNose)
+    leftEdge = [bboxFace(1),noseLeft(2)];
+end
+if ~exist("rightEdge")
+    disp(bboxNose)
+    rightEdge = [bboxFace(1) + bboxFace(3) ,noseRight(2)];
+end
+
+%face width
+
+faceWidth = rightEdge(1) - leftEdge(1) 
+
+
+
+
 
 function [widthOfFeature, heightOfFeature] = getSizeOfGrayscaleFeature(feature, mergeThresholdBool)
     % Select a certain facial feature.
