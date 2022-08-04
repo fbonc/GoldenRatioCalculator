528eb30653ffc5a6593fff2b054232aac54b89b3
Finnian Phelan <finnianphelan@outlook.com>
2022-08-04 16:13:18 -0400

    Added final calculation

--- main.m
+++ main.m
@@ -10,9 +10,13 @@ videoFilename = 'testvideos/video1.mp4';
 [faceWidth, faceHeight, bboxFace, bboxNose, chinLocation] = getFacialDimensions(videoFilename);
 
 % Final distance values
-pupilToLip = mouthLocation(2) - eyesLocation(2)
-noseToChin = chinLocation - noseLocation(2)
-lipsToChin = chinLocation - mouthLocation(2)
-pupilToNose = noseLocation(2) - eyesLocation(2)
-noseToLip = mouthLocation(2) - noseLocation(2)
-hairlineToPupil = eyesLocation(2) -  bboxFace(2)
+pupilToLip = mouthLocation(2) - eyesLocation(2);
+noseToChin = chinLocation - noseLocation(2);
+lipsToChin = chinLocation - mouthLocation(2);
+pupilToNose = noseLocation(2) - eyesLocation(2);
+noseToLips = mouthLocation(2) - noseLocation(2);
+hairlineToPupil = eyesLocation(2) -  bboxFace(2);
+
+overallValue = ((noseToChin / lipsToChin)+(noseToChin / pupilToNose)+(noseWidth / noseToLips)+(eyesWidth / hairlineToPupil)+(mouthHeight / noseWidth)) / 5;
+goldenRatioConstant = (1 + sqrt(5)) / 2;
+round(overallValue / goldenRatioConstant * 10, 1)
